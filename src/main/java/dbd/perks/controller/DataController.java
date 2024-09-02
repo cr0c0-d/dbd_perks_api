381c108faf0e3f701a4fb02217d8827dfd81041b
cr0c0 <hyde69ciel@gmail.com>
2024-09-02 20:12:25 +0900

    #240902 기존 TestController 삭제 및 DataController 생성, 크롤링 api 및 데이터 조회 api 추가

--- src/main/java/dbd/perks/controller/DataController.java
+++ src/main/java/dbd/perks/controller/DataController.java
@@ -1,14 +1,18 @@
 package dbd.perks.controller;
 
 import dbd.perks.crawler.DataCrawler;
+import dbd.perks.dto.WholeDataFindResponse;
+import dbd.perks.service.DataService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class TestController {
+public class DataController {
 
+    private final DataService dataService;
     private final DataCrawler dataCrawler;
 
     @GetMapping("/api/runKillerCrawler")
@@ -25,5 +29,11 @@ public class TestController {
     public void runOfferingCrawler() {
         dataCrawler.runOfferingCrawler();
     }
+
+    @GetMapping("/api/getData")
+    public ResponseEntity<WholeDataFindResponse> getData() {
+        return ResponseEntity.ok()
+                .body(dataService.getData());
+    };
 
 }
