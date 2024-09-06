056d2e2e4e59f331b6a9676f8ed5a81a277649c6
cr0c0 <hyde69ciel@gmail.com>
2024-09-06 18:40:52 +0900

    #240906 인터페이스 Data 객체 추가 - 각 도메인의 공통 부모 객체이며 isActivate 속성을 false로 바꾸는 deactivate 함수, 두 객체의 데이터를 비교하는 equals 함수를 가짐

---
+++ src/main/java/dbd/perks/domain/Data.java
@@ -0,0 +1,26 @@
+package dbd.perks.domain;
+
+
+import java.time.LocalDateTime;
+
+public interface Data {
+    Long id = null;
+
+    String name = null;
+
+    String enName = null;
+
+    Long ver = null;
+
+    LocalDateTime lastModified = null;
+
+    Boolean isActivated = null;
+
+    LocalDateTime createdAt = null;
+
+    Boolean equals(Data data);
+
+    void deactivate();
+
+    String getEnName();
+}
