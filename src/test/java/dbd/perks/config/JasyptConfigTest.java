48d3507f336df81b971f294e86f03fb578291902
cr0c0 <hyde69ciel@gmail.com>
2024-09-07 15:32:49 +0900

    #240907 테스트 코드 주석처리

--- src/test/java/dbd/perks/config/JasyptConfigTest.java
+++ src/test/java/dbd/perks/config/JasyptConfigTest.java
@@ -11,21 +11,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class JasyptConfigTest {
-    @Autowired
-    @Qualifier("jasyptStringEncryptor")
-    private StringEncryptor encryptor;
-
-    @Test
-    @DisplayName("Jasypt 암/복호화 테스트")
-    public void testJasypt() {
-        String value = "암호화할 텍스트";
-
-        String encrypted = encryptor.encrypt(value);
-        String decrypted = encryptor.decrypt(encrypted);
-
-        System.out.println("encrypted : " + encrypted + " / decrypted : " + decrypted);
-
-        assertThat(decrypted).isEqualTo(value);
-
-    }
+//    @Autowired
+//    @Qualifier("jasyptStringEncryptor")
+//    private StringEncryptor encryptor;
+//
+//    @Test
+//    @DisplayName("Jasypt 암/복호화 테스트")
+//    public void testJasypt() {
+//        String value = "암호화할 텍스트";
+//
+//        String encrypted = encryptor.encrypt(value);
+//        String decrypted = encryptor.decrypt(encrypted);
+//
+//        System.out.println("encrypted : " + encrypted + " / decrypted : " + decrypted);
+//
+//        assertThat(decrypted).isEqualTo(value);
+//
+//    }
 }
