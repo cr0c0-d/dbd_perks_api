715b5f50ee5cdc9b6b46a337413380deedea6be3
cr0c0 <hyde69ciel@gmail.com>
2024-09-25 12:53:34 +0900

    #240925 프록시 서버 추가

--- src/main/java/dbd/perks/crawler/ScrollCrawler.java
+++ src/main/java/dbd/perks/crawler/ScrollCrawler.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.nio.file.Paths;
@@ -17,6 +18,10 @@ import java.util.Map;
 
 @Service
 public class ScrollCrawler {
+
+    @Value("${proxy.server}")
+    private String proxyServer;
+
     public Document getDocumentByScrollCrawler(String url) {
         String os = System.getProperty("os.name").toLowerCase();
         ChromeOptions options = new ChromeOptions();
@@ -48,6 +53,9 @@ public class ScrollCrawler {
 
         // User-Agent 지정으로 봇 인식 회피
         options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
+
+        // 프록시 서버 추가
+        options.addArguments("--proxy-server=" + proxyServer);
 
         WebDriver driver = new ChromeDriver(options);
 
