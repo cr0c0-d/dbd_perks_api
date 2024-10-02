30afa5fc2898df0a9fffc052a20b5555afb87aa7
cr0c0 <hyde69ciel@gmail.com>
2024-10-02 23:08:29 +0900

    #241002 관리자 콘솔 페이지 - Playable 데이터 조회 페이지

--- src/main/java/dbd/perks/controller/AdminViewController.java
+++ src/main/java/dbd/perks/controller/AdminViewController.java
@@ -1,12 +1,26 @@
 package dbd.perks.controller;
 
+import dbd.perks.repository.PlayableRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AdminViewController {
+
+    private final PlayableRepository playableRepository;
+
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/playable")
+    public String getPlayerData(Model model) {
+        model.addAttribute("playables", playableRepository.findAll());
+
+        return "playable";
     }
 }
