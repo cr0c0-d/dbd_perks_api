32bc9d4d3f62db849944a036b85fe8812c346692
cr0c0 <hyde69ciel@gmail.com>
2024-10-03 19:42:31 +0900

    #241003 관리자 콘솔 페이지 - Playable 외 다른 도메인별 뷰컨트롤러 함수 추가

--- src/main/java/dbd/perks/controller/AdminViewController.java
+++ src/main/java/dbd/perks/controller/AdminViewController.java
@@ -1,6 +1,6 @@
 package dbd.perks.controller;
 
-import dbd.perks.repository.PlayableRepository;
+import dbd.perks.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AdminViewController {
 
     private final PlayableRepository playableRepository;
+    private final PerkRepository perkRepository;
+    private final AddonRepository addonRepository;
+    private final OfferingRepository offeringRepository;
+    private final ItemRepository itemRepository;
+    private final WeaponRepository weaponRepository;
 
     @GetMapping("/login")
     public String login() {
@@ -18,9 +23,44 @@ public class AdminViewController {
     }
 
     @GetMapping("/playable")
-    public String getPlayerData(Model model) {
+    public String getPlayableData(Model model) {
         model.addAttribute("playables", playableRepository.findAll());
 
         return "playable";
+    }
+
+    @GetMapping("/perk")
+    public String getPerkData(Model model) {
+        model.addAttribute("perks", perkRepository.findAll());
+
+        return "perk";
+    }
+
+    @GetMapping("/addon")
+    public String getAddonData(Model model) {
+        model.addAttribute("addons", addonRepository.findAll());
+
+        return "addon";
+    }
+
+    @GetMapping("/offering")
+    public String getOfferingData(Model model) {
+        model.addAttribute("offerings", offeringRepository.findAll());
+
+        return "offering";
+    }
+
+    @GetMapping("/item")
+    public String getItemData(Model model) {
+        model.addAttribute("items", itemRepository.findAll());
+
+        return "item";
+    }
+
+    @GetMapping("/weapon")
+    public String getWeaponData(Model model) {
+        model.addAttribute("weapons", weaponRepository.findAll());
+
+        return "weapon";
     }
 }
