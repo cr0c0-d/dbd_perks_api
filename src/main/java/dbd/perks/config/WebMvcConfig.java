a5d62cb6fdb26f93214c6e4623d8000f53b09d89
cr0c0 <hyde69ciel@gmail.com>
2024-10-19 02:44:42 +0900

    #241019 allowedOrigins 수정

--- src/main/java/dbd/perks/config/WebMvcConfig.java
+++ src/main/java/dbd/perks/config/WebMvcConfig.java
@@ -17,7 +17,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://dbd-random-perks-kr.netlify.app", allowed)
+                .allowedOrigins("https://dbd-random-perks-kr.netlify.app", "http://dbd-random-perks-kr.netlify.app", allowed)
                 .allowCredentials(true)
                 .allowedHeaders("Authorization")
                 .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
