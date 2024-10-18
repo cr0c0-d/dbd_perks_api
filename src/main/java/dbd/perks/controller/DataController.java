ad6049725d30d3f6e5239552a6209d22abcb185b
cr0c0 <hyde69ciel@gmail.com>
2024-10-19 04:25:25 +0900

    #241019 이미지 조회 테스트

--- src/main/java/dbd/perks/controller/DataController.java
+++ src/main/java/dbd/perks/controller/DataController.java
@@ -5,10 +5,17 @@ import dbd.perks.dto.WholeDataFindResponse;
 import dbd.perks.service.DataService;
 import dbd.perks.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Base64;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +26,8 @@ public class DataController {
 
     private final EmailService emailService;
 
+    @Value("${img.path}")
+    String imgPath;
 
     // 매일 자정에 실행
     @Scheduled(cron = "0 0 0 * * *")
@@ -52,4 +61,17 @@ public class DataController {
         return ResponseEntity.ok()
                 .body(dataService.getData());
     }
+
+    @GetMapping("/imgs/{name}")
+    public ResponseEntity<String> getImageBase64(@PathVariable String fileName) {
+        try {
+            File file = new File(imgPath + File.separator+ fileName);
+            byte[] fileContent = Files.readAllBytes(file.toPath());
+            String base64String = Base64.getEncoder().encodeToString(fileContent);
+            return ResponseEntity.ok("data:image/jpeg;base64," + base64String);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
 }
